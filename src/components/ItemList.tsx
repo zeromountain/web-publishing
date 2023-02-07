4452ccf3e68fa268181c21fb2b83f363f3551d73
zeromountain <sonyeongsan@gmail.com>
2023-02-07 09:53:58 +0900

    feat: 🎸Item List 컴포넌트

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,22 +1,100 @@
-import styled from '@emotion/styled';
-import React from 'react';
-import { Flex } from './common/Styles';
-import ProductImage from '../assets/img/product-01.png';
+import styled from "@emotion/styled";
+
+import GiraffeImage from "../assets/img/product-01.png";
+import HippoImage from "../assets/img/product-02.png";
+import RirakumaImage from "../assets/img/product-03.png";
 
 const ItemList = () => {
   return (
-    <Flex
-      height='400px'
-      alignItems='center'
-      justifyContent='center'
-      background='red'>
-      <img src={ProductImage} alt='doll' />
-    </Flex>
+    <Items>
+      <Item>
+        <img src={GiraffeImage} alt="doll" />
+        <Caption className="caption">
+          <h2>Giraffe doll 25cm</h2>
+          <p>
+            The owner of the mos popular and cute looks Rilakkuma ~! Rilakkuma
+            giant - It is size sale to small size.
+          </p>
+          <p>
+            Price : <s>$12</s> → $10
+          </p>
+          <a href="#none">View Details</a>
+        </Caption>
+      </Item>
+      <Item>
+        <img src={HippoImage} alt="doll" />
+        <Caption className="caption">
+          <h2>Hippo doll 25cm</h2>
+          <p>
+            The owner of the mos popular and cute looks Rilakkuma ~! Rilakkuma
+            giant - It is size sale to small size.
+          </p>
+          <p>
+            Price : <s>$12</s> → $10
+          </p>
+          <a href="#none">View Details</a>
+        </Caption>
+      </Item>
+      <Item>
+        <img src={RirakumaImage} alt="doll" />
+        <Caption className="caption">
+          <h2>Rirakuma doll 25cm</h2>
+          <p>
+            The owner of the mos popular and cute looks Rilakkuma ~! Rilakkuma
+            giant - It is size sale to small size.
+          </p>
+          <p>
+            Price : <s>$12</s> → $10
+          </p>
+          <a href="#none">View Details</a>
+        </Caption>
+      </Item>
+    </Items>
   );
 };
 
-const Items = styled.div``;
+const Items = styled.div`
+  text-align: center;
+`;
 
-const Item = styled.div``;
+const Item = styled.div`
+  display: inline-block;
+  width: 300px;
+  height: 300px;
+  position: relative;
+  margin: 10px;
+
+  &: hover .caption {
+    opacity: 1;
+  }
+
+  & > img {
+    border: 1px solid #eee;
+  }
+`;
+
+const Caption = styled.div`
+  opacity: 0;
+  transition: 0.5s;
+  width: 300px;
+  height: 300px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  color: #fff;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 20px;
+
+  & > a {
+    color: #fff;
+    background-color: teal;
+    padding: 7px;
+
+    &: hover {
+      background-color: #fff;
+      color: black;
+    }
+  }
+`;
 
 export default ItemList;
